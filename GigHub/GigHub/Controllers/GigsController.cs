cc56f55f29ecb4976feba62814cf806680dd45f7
nasr2ldin <nasr2ldin@gmail.com>
2016-08-21 19:51:21 +0300

    ValidateAntiForgeryToken

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -27,6 +27,7 @@ namespace GigHub.Controllers
         }
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             //var artist = _context.Users.Single(u => u.Id == artistId);
